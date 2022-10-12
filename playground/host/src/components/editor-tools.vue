a70ee920d5f0caf2ad870681a787c2c5cc0888b5
岳晓亮 <2220124666@qq.com>
2022-10-12 20:34:23 +0800

    feat(playground): playground 开发

--- playground/host/src/components/editor-tools.vue
+++ playground/host/src/components/editor-tools.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import { reactive, ref } from 'vue';
+import { computed, ref } from 'vue';
+import type { TableMergeSpanTypes, TableMergeOptions } from '@table-merge/core';
+import type { OptionsRange } from '@table-merge/core/dist/types/index';
 
 const data = [
   { id: 1, a: 8, b: 8, c: 2, d: 0 },
@@ -18,42 +20,73 @@ const columns = [
   { prop: 'd', label: 'D列', align: 'center' }
 ];
 
+interface FormData extends TableMergeOptions {
+  spanType: TableMergeSpanTypes;
+}
+
+const keys = ref<string[]>(allKeys.value);
+const spanType = ref<TableMergeSpanTypes>('rowSpan');
+const rangeStart = ref(0);
+const rangeEnd = ref(allKeys.value.length);
 const rangeType = ref('number');
-const formData = reactive({});
+const range = computed<OptionsRange>(() => {
+  const key = {
+    rowSpan: 'row',
+    colSpan: 'col'
+  }[spanType.value];
+  const value = computed(() => (rangeType.value === 'number' ? rangeStart : [rangeStart, rangeEnd]));
+  return {
+    [key]: value.value
+  };
+});
+
+const formData = computed(() => {
+  return {
+    spanType: spanType.value,
+    keys: allKeys.value,
+    range: range.value
+  };
+});
 </script>
 
 <template>
   <div class="editor-tools">
-    <el-form :model="formData" style="max-width: 460px">
-      <el-form-item label="Span Type" class="block">
-        <el-radio-group v-model="formData.spanType" class="ml-4">
-          <el-radio label="rowSpan">rowSpan</el-radio>
-          <el-radio label="colSpan">colSpan</el-radio>
+    <div class="block-title">配置项</div>
+    <el-form style="max-width: 460px">
+      <el-form-item label="合并方式：" class="block">
+        <el-radio-group v-model="spanType">
+          <el-radio label="rowSpan">合并行</el-radio>
+          <el-radio label="colSpan">合并列</el-radio>
         </el-radio-group>
       </el-form-item>
-      <el-form-item label="Keys" class="block">
-        <el-checkbox-group v-model="formData.keys">
+
+      <el-form-item label="合并的列：" class="block">
+        <el-checkbox-group v-model="keys">
           <el-checkbox v-for="key in allKeys" :key="key" :label="key" />
         </el-checkbox-group>
       </el-form-item>
-      <el-form-item label="Range Type">
-        <el-radio-group v-model="rangeType" class="ml-4">
-          <el-radio label="number">number</el-radio>
-          <el-radio label="array">array</el-radio>
+
+      <el-form-item label="合并范围：">
+        <el-radio-group v-model="rangeType">
+          <el-radio label="number">开始索引</el-radio>
+          <el-radio label="array">开始到结束索引</el-radio>
         </el-radio-group>
       </el-form-item>
+
       <el-form-item>
         <template v-if="rangeType === 'number'">
-          <el-input-number v-model="formData.range" :min="1" :max="10" />
+          <el-input-number v-model="rangeStart" :min="1" :max="10" />
         </template>
+
         <template v-if="rangeType === 'array'">
           <el-col :span="11">
-            <el-input-number v-model="formData.range" :min="1" :max="10" style="width: 100%" />
+            <el-input-number v-model="rangeStart" :min="1" :max="10" style="width: 100%" />
           </el-col>
           <el-col class="text-center" :span="1" style="text-align: center">-</el-col>
           <el-col :span="11">
-            <el-input-number v-model="formData.range" :min="1" :max="10" style="width: 100%" />
+            <el-input-number v-model="rangeEnd" :min="1" :max="10" style="width: 100%" />
           </el-col>
+          <p class="remark">ps: 包含开始，不包含结束</p>
         </template>
       </el-form-item>
     </el-form>
@@ -67,6 +100,11 @@ const formData = reactive({});
   padding: var(--editor-padding);
   border-right: 1px solid var(--border-color);
   box-sizing: border-box;
+
+  .remark {
+    width: 100%;
+    color: var(--tools-remark-color);
+  }
 }
 
 .block {
